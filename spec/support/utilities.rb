4dc305e8893c71472770c1639ef5921c79a42bbf
Damon Beaven <beaven@lexmark.com>
2014-01-31 13:56:34 -0500

    chapter 5 exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,12 @@
-def full_title (page_title)
-  #let's create a title.  if we get a page_title we will add it to the full_title
-  #otherwise we just return the full_title.
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+#def full_title (page_title)
+#  #let's create a title.  if we get a page_title we will add it to the full_title
+#  #otherwise we just return the full_title.
+#  base_title = "Ruby on Rails Tutorial Sample App"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end
