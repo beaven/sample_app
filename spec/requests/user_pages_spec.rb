880b347bfb04849188c5cf6ccab70c4a949739af
Damon Beaven <beaven@lexmark.com>
2014-02-05 15:50:42 -0500

    chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,6 +32,14 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit}
+
+        it {should have_title(full_title('Sign up')) }
+        it {should have_content('error')}
+      end
+
     end
 
     describe "with valid information" do
@@ -44,6 +52,18 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        # try to find the user we just created
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        # verify that the page has the correct user information
+        # does the name match?
+        it { should have_title(user.name) }
+        # did the page say success?
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
